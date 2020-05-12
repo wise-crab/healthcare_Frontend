c81f6a879e66deaa825a4eb112342d53938595ed
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-12 13:57:51 -0500

    [React][babel]

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <h1>This is the main ExaMedic's Component</h1>
+)
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
