32795cbdc43f4e1c19ca4cfcc2467699470adda9
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-12 14:21:37 -0500

    [webpack]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
