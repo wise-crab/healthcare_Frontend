5e43dac32cd27e130ad37646720f727e9c560e69
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-05-19 22:53:29 -0500

    [Webpack] update webpackconfig for fully css and sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  devServer:{
+  devServer: {
     hot: true,
     open: true,
     port: 8080,
   },
-  module:{
+  module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
@@ -29,13 +30,11 @@ module.exports = {
         test: /\.html$/,
         use: {
           loader: 'html-loader',
-        }
+        },
       },
       {
         test: /\.(s*)css$/,
-        use: [
-          'sass-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png)$/,
@@ -43,16 +42,19 @@ module.exports = {
           {
             loader: 'file-loader',
             options: { name: 'assets/[hash].[ext]' },
-          }
-        ]
-      }
-    ]
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: './src/assets/[name].css',
+    }),
     new webpack.HotModuleReplacementPlugin(),
   ],
-}
+};
