30d9f68a032b0be34f272c28c6175beb46d4199d
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-05-20 01:20:11 -0500

    [SASS] add variables and base styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from '../views/Login';
+import '../assets/styles/sass/styles.scss';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/'component={Login} />
+    <Route exact path='/' component={Login} />
   </BrowserRouter>
-)
+);
 
 export default App;
