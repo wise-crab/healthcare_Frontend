c5c4c64e4035d323b5e94c1e39c3a1eb8679fee8
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-20 17:49:49 -0500

    skip this commit

---
+++ src/views/Layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/Header'
+import Sidebar from '../../components/Sidebar'
+import Layout from '../'
+
+const Home = () => {
+  return (
+
+  );
+}
