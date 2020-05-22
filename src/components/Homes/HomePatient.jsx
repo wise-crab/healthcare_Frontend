4e95f0a649f66266a0c8102de488e4541dd5fca4
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-22 14:49:49 -0500

    [Table] My own wy to develop the table

--- src/components/Homes/HomePatient.jsx
+++ src/components/Homes/HomePatient.jsx
@@ -1,42 +1,25 @@
 import React, { Component } from 'react';
+import '../../assets/styles/sass/components/__table.scss'
 
-import { AgGridReact } from 'ag-grid-react';
-
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-
-export class HomePatient extends Component {
+class HomePatient extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      columnDefs: [
-        { headerName: 'Select', field: 'checkbox', checkboxSelection: true},
-        { headerName: 'Type Of Exam', field: 'type' },
-        { headerName: 'Order Date', field: 'date' },
-        { headerName: 'Order ID', field: 'id' },
-        { headerName: 'Status', field: 'action' }
-      ],
-      rowData: [
-        { type: 'Celica', date: 35000, id: 'any', action: 'avaliable' },
-        { type: 'Mondeo', date: 32000, id: 'any', action: 'avaliable' },
-        { type: 'Boxter', date: 72000, id: 'any', action: 'avaliable' }]
-    }
+    this.state = {}
   }
 
   render() {
     return (
-      <div className='ag-theme-alpine' style={ {height: '200px', width: '1fr'} }>
-      <button className='button' onClick={this.onButtonClick}>Get selected rows</button>
-      <AgGridReact
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}
-          rowSelection='multiple'
-          >
-      </AgGridReact>
-    </div>
+      <div className='table'>
+        <div className='table__title'>
+          <div className="table__title-def">Select</div>
+          <div className="table__title-def">Type Of Exam</div>
+          <div className="table__title-def">Order Date</div>
+          <div className="table__title-def">Order ID</div>
+          <div className="table__title-def">Status</div>
+        </div>
+      </div>
     )
   }
 }
 
 export default HomePatient
-
