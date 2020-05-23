d6c10fa0f2e311413f98ac888c4d228bd3775656
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-05-22 23:59:10 -0500

    [Views][Patient] patient profile view, full responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from '../views/Login';
 import Layout from '../views/Layout';
+import Profile from '../views/Profile';
 
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Route exact path='/' component={Login} />
+      <Route exact path='/profile' component={Profile} />
     </Layout>
   </BrowserRouter>
 );
