77de510d1a8117927960a6abba644f3c50bc0ca2
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-05-23 19:48:54 -0500

    [Table] update table with hooks

--- src/components/Homes/HomePatient.jsx
+++ src/components/Homes/HomePatient.jsx
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../assets/styles/sass/components/__table.scss';
-import table from '../Table'
 import Table from '../Table';
 
-import getData from '../../functions/getData'
+const HomePatient = () => {
+  const [state, setState] = useState({ isReady: false, data: [] });
+  useEffect(() => {
+    fetch('https://data-mock-278118.wl.r.appspot.com/api/exams')
+      .then((response) => response.json())
+      .then((data) => {
+        setState({ isReady: true, data: data.data });
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-const API = 'http://0.0.0.0:3000/exams'
-
-const HomePatient =() => {
-  const data = getData();
-  return (
-    <Table data={data}/>
-  )
-}
-
+  if (!state.isReady) {
+    return 'cargando';
+  }
+  return <Table data={state.data} />;
+};
 export default HomePatient;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,41 +1,38 @@
 import React, { Component } from 'react';
 import '../assets/styles/sass/components/__table.scss';
 
-const Table =({ data }) => {
+const Table = ({ data }) => {
   const titles = Object.keys(data[0]);
-  
+
   return (
     <div className='table'>
       <div className='table__title'>
-        {
-          titles.map((title,idx) => {
-            console.log(title);
-            return(
-              <div className='table__title def' key={idx}>
-                <h5>{title}</h5>
-              </div>
-            )
-          })
-        }
-      
+        {titles.map((title, idx) => {
+          console.log(title);
+          return (
+            <div className='table__title def' key={idx}>
+              <h5>{title}</h5>
+            </div>
+          );
+        })}
       </div>
       <div className='table__content'>
-        {data.map((item,idx) =>{
-          const date = new Date(item.date *1000).toDateString();
-          
+        {data.map((item, idx) => {
+          const date = new Date(item.date * 1000).toDateString();
+
           return (
-              <div key={idx} className='table__content-item'>
-                <div className='table__item def'>Select</div>
-                <div className='table__item def'>{item.type}</div>
-                <div className='table__item def'>{date}</div>
-                <div className='table__item def'>{item.$oid}</div>
-                <div className='table__item def'>{item.status}</div>
+            <div key={idx} className='table__content-item'>
+              <div className='table__item def'>Select</div>
+              <div className='table__item def'>{item.type}</div>
+              <div className='table__item def'>{date}</div>
+              <div className='table__item def'>{item.$oid}</div>
+              <div className='table__item def'>{item.status}</div>
             </div>
-          )}
-        )}            
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
