e2574bf13d5947bc13f474793ade8fa25e5be5d6
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-24 15:56:08 -0500

    [Home] convert to statefull component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Doctor from '../components/Profiles/Doctor';
 import Patient from '../components/Profiles/Patient';
 import Admin from '../components/Profiles/Admin';
 import Bacteriologist from '../components/Profiles/Bacteriologist';
+import { getUserByID } from '../APIS/apis'
+
+const api = getUserByID;
                               
-const Home = () => {
-  const role = 'bacteriologist';
-  return (
-    <section className='doctor card'>
-      {role == 'patient' && <Patient />}
-      {role == 'doctor' && <Doctor />}
-      {role == 'admin' && <Admin />}
-      {role == 'bacteriologist' && <Bacteriologist />}
-    </section>
-  );
+class Home extends Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData = async ()=>{
+    this.setState({ loading: true, error: null })
+    try {
+      const role = await api
+      this.setState({ loading: false, data: data });
+    } catch (err) {
+      this.setState({ loading: false, error: error })
+    }
+  }
+
+  render(){
+
+
+    return (
+      <section className='doctor card'>
+        {role == 'patient' && <Patient />}
+        {role == 'doctor' && <Doctor />}
+        {role == 'admin' && <Admin />}
+        {role == 'bacteriologist' && <Bacteriologist />}
+      </section>
+    );
+
+  }
 };
 export default Home;
