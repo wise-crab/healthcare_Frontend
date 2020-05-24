ab2d0b7be7304e25bd0c4b329d431e33554112a0
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-24 17:33:16 -0500

    [Home] Home becomes a stateful component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,32 +4,56 @@ import Patient from '../components/Profiles/Patient';
 import Admin from '../components/Profiles/Admin';
 import Bacteriologist from '../components/Profiles/Bacteriologist';
 import { getUserByID } from '../APIS/apis'
-
-const api = getUserByID;
                               
 class Home extends Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
-
-  componentDidMount(){
-    this.fetchData();
-  }
-
-  fetchData = async ()=>{
-    this.setState({ loading: true, error: null })
-    try {
-      const role = await api
-      this.setState({ loading: false, data: data });
-    } catch (err) {
-      this.setState({ loading: false, error: error })
+  constructor(){
+    super();
+    this.state = {
+      "users" : [
+        {
+        "$oid": "5ec6c34efc13ae0f3a000028",
+        "cdi": "1994843918",
+        "name": "Sascha",
+        "last_name": "Pettifer",
+        "email": "spettifer0@tuttocitta.it",
+        "phone": "551-312-2514",
+        "role": "doctor"
+        },
+        {
+        "$oid": "5ec6c34efc13ae0f3a000029",
+        "cdi": "6576698690",
+        "name": "Lettie",
+        "last_name": "Rosekilly",
+        "email": "lrosekilly1@jigsy.com",
+        "phone": "564-700-1097",
+        "role": "patient"
+        },
+        {
+        "$oid": "5ec6c34efc13ae0f3a00002a",
+        "cdi": "5025601703",
+        "name": "Clemence",
+        "last_name": "Streeting",
+        "email": "cstreeting2@friendfeed.com",
+        "phone": "484-441-1672",
+        "role": "bacteriologist"
+        },
+        {
+          "$oid": "5ec6c34efc13ae0f3a00002c",
+          "cdi": "0129194190",
+          "name": "Petrina",
+          "last_name": "Causbey",
+          "email": "pcausbey4@tinyurl.com",
+          "phone": "142-427-3406",
+          "role": "admin"
+        }
+      ]
     }
   }
-
+  
+  
   render(){
-
+    
+    const role = this.state.users[0].role;
 
     return (
       <section className='doctor card'>
