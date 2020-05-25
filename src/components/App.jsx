c7b43baa05ff388bb9239a2f82f9a4ed3695aba1
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2020-05-25 18:38:05 -0500

    Revert "implement file upload"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,21 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Login from "../views/Login";
-import Home from "../views/Home";
-import Layout from "../views/Layout";
-import Profile from "../views/Profile";
-import CreateUser from "../views/CreateUser";
-import BulkData from "../views/BulkData";
-import NotFound from "../views/NotFound";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../views/Login';
+import Home from '../views/Home';
+import Layout from '../views/Layout';
+import Profile from '../views/Profile';
+import CreateUser from '../views/CreateUser';
+import NotFound from '../views/NotFound';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/create-user" component={CreateUser} />
-          <Route exact path="/bulk-data" component={BulkData} />
+          <Route exact path='/' component={Login} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/create-user' component={CreateUser} />
           <Route component component={NotFound} />
         </Switch>
       </Layout>
