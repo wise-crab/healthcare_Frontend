16956bd0357109a6ec2500c5e902832c6ece4cf3
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-05-29 17:24:44 -0500

    [Store] add redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
-import reducers from './reducers'
+import reducers from './reducers';
 import App from './components/App';
 
-const store = createStore(reducers,{},applyMiddleware(reduxThunk));
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
